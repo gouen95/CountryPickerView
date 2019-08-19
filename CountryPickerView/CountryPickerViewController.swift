482ddd86c00425467b0e6bdc5feae9293f13aa3a
Brandon Wong Ka Seng <ks.wong@ssc2u.com>
2019-08-19 13:35:57 +0800

    omegalul

--- CountryPickerView/CountryPickerViewController.swift
+++ CountryPickerView/CountryPickerViewController.swift
@@ -78,7 +78,10 @@ extension CountryPickerViewController {
         
         if #available(iOS 13.0, *) {
             tableView.backgroundColor = .systemBackground
+        } else {
+            tableView.backgroundColor = .clear
         }
+        tableView.isOpaque = false
         tableView.backgroundView = nil
         tableView.sectionIndexBackgroundColor = .clear
         tableView.sectionIndexTrackingBackgroundColor = .clear
@@ -198,11 +201,12 @@ extension CountryPickerViewController: UITableViewDataSourcePrefetching {
             
             var index = 1
             repeat {
+                print("\(index) vs \(max(0,(self.countries[self.sectionsTitles[indexPaths.last!.section]]!.count - indexPaths.last!.row - 2)))")
                 mutableIndexPaths.append(IndexPath(row: indexPaths.last!.row + index, section: indexPaths.last!.section))
                 index += 1
             } while index < max(0,(self.countries[self.sectionsTitles[indexPaths.last!.section]]!.count - indexPaths.last!.row - 2)) //- 2 cuz index = 1 and count must - 1
             
-            for indexPath in mutableIndexPaths {
+            for indexPath in indexPaths {
                 self.countries[self.sectionsTitles[indexPath.section]]![indexPath.row].prepareFlagIfNeeded() 
             }
         }
